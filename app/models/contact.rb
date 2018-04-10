34a5c9b66734be543506e2a0fda0849ff3017865
Alexis Pipieri <alexis.pipieri@gmail.com>
2018-04-10 09:21:54 +0200

    setting up pg search

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,14 @@ class Contact < ApplicationRecord
   belongs_to :user
   has_many :actions, dependent: :destroy
   has_many :notes, dependent: :destroy
+
+  include PgSearch
+    pg_search_scope :global_search,
+      against: [ :first_name, :last_name ],
+      associated_against: {
+        organization: [ :name ]
+      },
+      using: {
+        tsearch: { prefix: true }
+      }
 end
